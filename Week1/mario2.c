0a22946c3e3eaa349901ccf8cd074f8829bfa32c
Meiqian Zheng <meiqianz@Christophs-MBP.office.5minds.de>
2019-01-09 16:50:12 +0100

    Solution

--- Week1/mario2.c
+++ Week1/mario2.c
@@ -4,37 +4,37 @@
 void hashtag_matrix2(int num_row){
     
     int num_col = 2*num_row+2;
-    char Hashtags[num_row][num_col];
+    char Hashtags[num_col];
     
     for(int line=0; line<num_row; line++){
         //left matrix
         for(int col=0; col<num_row; col++){
-            if( (line+col)>=num_row-1 ){
-                Hashtags[line][col]='#';
+            int linepluscol =line+col;
+            if( linepluscol<num_row-1 ){
+                Hashtags[col]=' ';
             }else{
-                Hashtags[line][col]=' ';
+                Hashtags[col]='#';
             }
         }
         //middle of matrix
         for(int col=num_row; col<num_row+2; col++){
-            Hashtags[line][col]=' ';
+            Hashtags[col]=' ';
         }
         //right matrix
         for(int col=num_row+2; col<num_col; col++){
             if( (col-num_row-2) <=line ){
-                Hashtags[line][col]='#';
+                Hashtags[col]='#';
             }else{
-                Hashtags[line][col]=' ';
+                Hashtags[col]=' ';
             }
         }
        
-        printf("%s\n", Hashtags[line]);
+        printf("%s\n", Hashtags);
     }
 }
 
 int main(void){
-    printf("Height: ");
-    int height = get_int();
+    int height = get_int("Height: ");
     if (height>0){
         hashtag_matrix2(height);
     }

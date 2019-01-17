b241533822059acabf747cb113aa46f7c399f4b5
Meiqian Zheng <meiqianz@Christophs-MBP.office.5minds.de>
2019-01-17 12:42:06 +0100

    Improvement

--- Week1/cash.c
+++ Week1/cash.c
@@ -4,27 +4,25 @@
 void cashback(int);
 
 int main(void){
-
-    float owe = get_float("Change owed: ");
-    int returnPenny = (int)(owe*100);
-    if (returnPenny>0){
-
-        cashback(returnPenny);
-    }else{
-
-        printf("foo\n");//requires
-    }
+    
+    float oweChange;
+    do {
+        oweChange = get_float("Change owed: ");
+    } while ( oweChange <= 0 );
+    
+    int returnPenny = (int) (oweChange * 100);
+    cashback(returnPenny);
 }
 
 void cashback(int returnPenny){
 
     int numberOfCoinTyp = 4;
-    int coinTyp[numberOfCoinTyp] = {25, 10, 5, 1};
+    int coinTyp[4] = {25, 10, 5, 1};
     int amountOfReturnCoints = 0;
-    for (int i=0; i<numberOfCoinTyp && returnPenny>0; i++){
+    for ( int i=0; i<numberOfCoinTyp && returnPenny>0; i++ ){
 
         amountOfReturnCoints +=  returnPenny / coinTyp[i];
-        returnPenny %= cointyp[i];
+        returnPenny %= coinTyp[i];
     }
 
     printf("%i\n", amountOfReturnCoints);

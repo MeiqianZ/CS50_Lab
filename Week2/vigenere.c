c49c404cd79b459d9175456fef48845041ae28cb
Meiqian Zheng <meiqianz@Christophs-MBP.office.5minds.de>
2019-01-18 11:22:08 +0100

    Improvement

--- Week2/vigenere.c
+++ Week2/vigenere.c
@@ -1,63 +1,70 @@
- #include <cs50.h>
+#include <cs50.h>
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 
-int key();
-void vigeneresCipher();
+void vigeneresCipher(int Key[], int length);
 
-int main(int argc, string argv[])
-{
-    if (argc==2){
+int main(int argc, string argv[]){
+    
+    if ( argc != 2 ){
         
-        bool alphabet = true;
+        printf("Usage: ./vigenere keyword\n");
+    } else {
+        
+        bool keyword = true;
         int Input[strlen(argv[1])];
-        int length = strlen(argv[1]);
-        
-        for (int j=0; j<length; j++){
+        int keywordLength = strlen(argv[1]);
+        for ( int keywordIndex = 0 ; keywordIndex < keywordLength ; keywordIndex++ ){
             
-            Input[j] = (int)argv[1][j];
-            
-            if (65<=Input[j] && Input[j]<=90){
-                Input[j] = Input[j]-65;
-            }else if(97<=Input[j] && Input[j]<=122){
-                Input[j] = Input[j]-97;
-            }else{
-                alphabet = false;
+            Input[keywordIndex] = (int) argv[1][keywordIndex];
+            if ( 65 <= Input[keywordIndex]  &&  Input[keywordIndex] <= 90 ){
+                
+                Input[keywordIndex] = Input[keywordIndex] - 65;
+            } else if ( 97 <= Input[keywordIndex]  &&  Input[keywordIndex] <= 122 ){
+                
+                Input[keywordIndex] -= 97;
+            } else {
+                
+                keyword = false;
                 printf("Usage: ./vigenere keyword\n");
                 break;
             }
         }
         
-        if (alphabet){
-            vigeneresCipher(Input, length); 
+        if ( keyword ){
+            
+            vigeneresCipher(Input, keywordLength); 
         }
-        
-    }else{
-        printf("Usage: ./vigenere keyword\n");
     }
 }
 
-void vigeneresCipher(int Key[], int Len){
+void vigeneresCipher(int Key[], int keywordLength){
     
     string plaintext = get_string("plaintext: ");
     int pLength = strlen(plaintext);
+    printf("pLength: %i\n", pLength);
     int plain[pLength];
-    char ciphertext[pLength];
-    
-    for(int i=0; i<pLength; i++){
+    char ciphertext[pLength+1];//end with '\0'
+    int plainIndex;
+    for ( plainIndex = 0 ; plaintext[plainIndex] != 0 ; plainIndex++ ){
         
         //to ASCII Code
-        plain[i] = (int)plaintext[i];
+        plain[plainIndex] = (int) plaintext[plainIndex];
         
         //wrapping around alphabetically with reuse of keys
-        if (65<=plain[i] && plain[i]<=90){
-            plain[i] = (plain[i]-65+Key[i%Len])%26+65;
-        }else if (97<=plain[i] && plain[i]<= 122){
-            plain[i] = (plain[i]-97+Key[i%Len])%26+97;
+        if ( 65 <= plain[plainIndex]  &&  plain[plainIndex] <= 90 ){
+            
+            plain[plainIndex] = (plain[plainIndex] - 65 + Key[plainIndex%keywordLength]) % 26 + 65;
+        } else if ( 97 <= plain[plainIndex]  &&  plain[plainIndex] <= 122 ){
+            
+            plain[plainIndex] = (plain[plainIndex] - 97 + Key[plainIndex%keywordLength]) % 26 + 97;
         }
         
-        ciphertext[i] = (char)(plain[i]); 
+        ciphertext[plainIndex] = (char) (plain[plainIndex]); 
     }
+    
+    ciphertext[plainIndex] = '\0';
     printf("ciphertext: %s\n", ciphertext);
 }
+ 

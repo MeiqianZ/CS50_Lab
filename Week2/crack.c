69171e7e33412b2358e7bd65879995204b342773
Meiqian Zheng <meiqianz@Christophs-MBP.office.5minds.de>
2019-01-18 10:54:13 +0100

    Improvement

--- Week2/crack.c
+++ Week2/crack.c
@@ -3,6 +3,7 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
+
 /*
  * brian:51.xJagtPnb6s--->TF
  * bjbrown:50GApilQSG3E2--->UPenn
@@ -18,23 +19,28 @@
  * zamyla:50cI2vYkF0YU2--->LOL
  * */
 
-int main(int argc, string argv[])
-{
-    if (argc==2){
+int main(int argc, string argv[]){
+    
+    if ( argc != 2 ){
+        
+        printf("Usage: ./crack HashedPassword\n");
+        return 1; //with main returning 1 (thereby signifying an error).       
+    } else {
         
         //password is no longer than five: a\0\0\0\0\0-ZZZZZ\0
         //string letters = "\0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         //letter frequency
         string letters = "\0eEtTaAoOiInNsShHrRdDlLcCuUmMwWfFgGyYpPbBvVkKjJxXqQzZ";//e\0\0\0\0\0-ZZZZZ\0
         int let_length = 53;//1+26+26 '\0'+letters
-        char salt[3] = {argv[1][0],argv[1][1],'\0'};
+        char salt[3] = {argv[1][0], argv[1][1], '\0'};
         // a\0\0\0\0\0-ZZZZZ\0->6
         char key[6] = "\0\0\0\0\0\0";
-        for (int pos5=0; pos5<let_length; pos5++){//from \0
-            for (int pos4=0; pos4<let_length; pos4++){//from \0
-                for (int pos3=0; pos3<let_length; pos3++){//from \0
-                    for (int pos2=0; pos2<let_length; pos2++){//from \0
-                        for (int pos1=1; pos1<let_length; pos1++){//from a
+        
+        for ( int pos5 = 0 ; pos5 < let_length ; pos5++ ){//from \0
+            for ( int pos4 = 0 ; pos4 < let_length ; pos4++ ){//from \0
+                for ( int pos3 = 0 ; pos3 < let_length ; pos3++ ){//from \0
+                    for ( int pos2 = 0 ; pos2 < let_length ; pos2++ ){//from \0
+                        for ( int pos1 = 1 ; pos1 < let_length ; pos1++ ){//from a
                             
                             //password is no longer than five (5) characters
                             key[0] = letters[pos1];
@@ -44,7 +50,8 @@ int main(int argc, string argv[])
                             key[4] = letters[pos5];
                             
                             //Compare created encryption with encryption of command line
-                            if (strcmp(crypt(key,salt),argv[1])==0){
+                            if ( strcmp(crypt(key,salt), argv[1]) == 0 ){
+                                
                                 printf("%s\n", key);
                                 return 0;
                             }
@@ -56,10 +63,6 @@ int main(int argc, string argv[])
         
         printf("Not found!\n");
         return 2;
-            
-    }else{
-        printf("Usage: ./crack HashedPassword\n");
-        return 1;
     }
 }
-
+ 

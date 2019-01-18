5527b9d6226e28deb0d8bdcc39630f8b0c39bb22
Meiqian Zheng <meiqianz@Christophs-MBP.office.5minds.de>
2019-01-18 10:42:43 +0100

    Improvement

--- Week2/caesar.c
+++ Week2/caesar.c
@@ -3,45 +3,50 @@
 #include <string.h>
 #include <math.h>
 
-int key();
-void caesarsCipher();
+int key(int Input[], int length);
+void caesarsCipher(int Key);
 
-int main(int argc, string argv[])
-{
-    if (argc==2){
+int main(int argc, string argv[]){
+    
+    if ( argc != 2 ){
+        
+        printf("Usage: ./caesar key\n");
+    } else {
         
         bool numeral = true;
         int Input[strlen(argv[1])];
         int length = strlen(argv[1]);
         
-        for (int j=0; j<length; j++){
+        for ( int inputIndex = 0 ; inputIndex < length ; inputIndex++ ){
             
-            Input[j] = (int)argv[1][j];
+            Input[inputIndex] = (int) argv[1][inputIndex];
             
-            if (48<=Input[j] && Input[j]<=57){
-                Input[j] = Input[j]-48;
-            }else{
-                numeral =false;
+            if ( 48 <= Input[inputIndex]  &&  Input[inputIndex] <= 57 ){
+                
+                Input[inputIndex] = Input[inputIndex] - 48;
+            } else {
+                
+                numeral = false;
                 printf("Usage: ./caesar key\n");
                 break;
             }
         }
         
-        if (numeral){
+        if ( numeral ){
+            
             caesarsCipher(key(Input, length)); 
         }
-        
-    }else{
-        printf("Usage: ./caesar key\n");
-    }
+    } 
 }
 
-int key(int inp[], int len){
-    int k=0;
-    for (int i=1; i<=len; i++){
-        k = inp[len-i]*pow(10,i-1)+k;
+int key(int Input[], int length){
+    
+    int Key = 0;
+    for ( int i = 1 ; i <= length ; i++ ){
+        
+        Key = Input[length-i] * pow(10,i-1) + Key;
     }
-    return k;
+    return Key;
 }
 
 void caesarsCipher(int Key){
@@ -51,18 +56,21 @@ void caesarsCipher(int Key){
     int plain[pLength];
     char ciphertext[pLength];
     
-    for(int i=0; i<pLength; i++){
+    for ( int i = 0 ; i < pLength ; i++ ){
         
-        plain[i] = (int)plaintext[i];
+        plain[i] = (int) plaintext[i];
         
         //wrapping around alphabetically
-        if (65<=plain[i] && plain[i]<=90){
-            plain[i] = (plain[i]-65+Key)%26+65;
-        }else if (97<=plain[i] && plain[i]<= 122){
-            plain[i] = (plain[i]-97+Key)%26+97;
+        if ( 65 <= plain[i]  &&  plain[i] <= 90 ){
+            
+            plain[i] = (plain[i] - 65 + Key) % 26 + 65;
+        } else if ( 97 <= plain[i]  &&  plain[i] <= 122 ){
+            
+            plain[i] = (plain[i] - 97 + Key) % 26 + 97;
         }
         
         ciphertext[i] = (char)(plain[i]); 
     }
     printf("ciphertext: %s\n", ciphertext);
 }
+ 
